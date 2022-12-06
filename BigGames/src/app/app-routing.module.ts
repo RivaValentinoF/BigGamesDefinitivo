8c310487d1055b270d63fefc70f77aab5b3e4f48
Nabb0 <rivavalentino.f@iisgalvanimi.edu.it>
2022-12-06 07:41:22 +0000

    turboTole

--- BigGames/src/app/app-routing.module.ts
+++ BigGames/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ricercashop } from './ricercashop/ricercashop.component';
+import { RicercashopComponent } from './ricercashop/ricercashop.component';
+import { RouterModule,Routes } from '@angular/router';
 
+const routes: Routes = [
+  { path: 'Ricercashop', component:  RicercashopComponent }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
