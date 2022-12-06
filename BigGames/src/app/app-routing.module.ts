442ec1644e147a2ac309f1aae7157db031d1e7d3
Nabb0 <rivavalentino.f@iisgalvanimi.edu.it>
2022-12-06 07:58:14 +0000

    siummete

--- BigGames/src/app/app-routing.module.ts
+++ BigGames/src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RicercashopComponent } from './ricercashop/ricercashop.component';
 import { RouterModule,Routes } from '@angular/router';
+import { GiochishopComponent } from './giochishop/giochishop.component';
 
 const routes: Routes = [
-  { path: 'Ricercashop', component:  RicercashopComponent }
+  { path: 'Giochishop', component:  GiochishopComponent }
 ];
 
 @NgModule({
